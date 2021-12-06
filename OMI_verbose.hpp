e2ad8454282ee8fab76fc58d8c0e7b4018d673be
Liton Barman <litonbarman2410@gmail.com>
2021-12-06 15:01:43 +0530

    Update OMI_verbose.hpp

--- OMI_verbose.hpp
+++ OMI_verbose.hpp
@@ -1,3 +1,24 @@
+/*
+------------------------------------------------
+ Open Model Importer (OMI)
+------------------------------------------------
+ Author : Liton Barman
+  
+ This software is License under GNU General Public License
+ version 3 ( GPLv3 )
+ 
+ Redistribution and use of this software in source and binary forms, 
+ with or without modification, are permitted provided that 
+ the following conditions are met:
+ 
+ . The source code must be made public whenever a distribution of
+   the software is made.
+ . Modifications of the software must be realised under the same license
+ . Changes made to the Source Code must be documented
+
+
+*/
+
 #ifndef __VERBOSE_H__
 
 #define __VERBOSE_H__
